0527f0a3eb8e82aa9ff42b6e4393b169c785867f
tatsuya ichikawa <ichikawa@tejimaya.com>
2013-02-18 18:21:58 +0900

    dispatchでcssとjsを読み込ませたので要らないファイル削除

--- apps/pc_frontend/modules/like/templates/_likeCommunity.php
+++
@@ -1,8 +0,0 @@
-<script id="LikelistTemplate" type="text/x-jquery-tmpl">
-  <table style="border: 1px #000 solid;">
-    <tr style="padding: 2px;">
-      <td style="width: 48px; padding: 2px;"><a href="${profile_url}"><img src="${profile_image}" width="48"></a></td>
-      <td style="padding: 2px;"><a href="${profile_url}">${name}</a></td>
-    </tr>
-  </table>
-</script>
