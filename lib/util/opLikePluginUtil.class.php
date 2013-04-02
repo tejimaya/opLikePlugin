b1a6cff63d4836a451385c60aca5afb436fbd903
kaoru nishizoe <nishizoe@tejimaya.com>
2013-04-02 17:34:42 +0900

    (6205#note-16) 国際化対応時にヘルパーを呼んでいたため、呼ばないように修正しました。

--- lib/util/opLikePluginUtil.class.php
+++ lib/util/opLikePluginUtil.class.php
@@ -4,8 +4,7 @@ class opLikePluginUtil
 {
   public static function sendNotification($fromMember, $toMember, $url = null)
   {
-    sfApplicationConfiguration::getActive()->loadHelpers(array('I18N'));
-    $message = __('%member% says that it is Like.', array('%member%' => $fromMember->getName()));
+    $message = sfContext::getInstance()->getI18n()->__('%member% says that it is Like.', array('%member%' => $fromMember->getName()));
 
     opNotificationCenter::notify($fromMember, $toMember, $message, array('category' => 'other', 'url' => $url, 'icon_url' => null));
   }
