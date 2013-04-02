197f594ef1f62a43b9f24029c836dd33c87af451
kaoru nishizoe <nishizoe@tejimaya.com>
2013-04-02 17:27:06 +0900

    (6205#note-12, 15) アプリケーション名の取得方法を変更し、pc_frontendのみ処理が実行されるよう修正しました。

--- lib/event/opLikeEvent.class.php
+++ lib/event/opLikeEvent.class.php
@@ -15,8 +15,7 @@ class opLikeEvent
     $repuest = sfContext::getInstance()->getRequest();
     $response = sfContext::getInstance()->getResponse();
 
-    $pos = strpos($_SERVER['SCRIPT_NAME'], 'pc_backend');
-    if (false === $pos)
+    if ('pc_frontend' === sfConfig::get('sf_app'))
     {
       $memberInstance = sfContext::getInstance()->getUser()->getMember();
       if (get_class($memberInstance) !== 'opAnonymousMember')
