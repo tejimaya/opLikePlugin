433bb84a05becfc6ab49ef761c9cd6d4638407da
Kimura Youichi <yoichi.kimura@tejimaya.com>
2016-09-09 19:26:24 +0900

    delete strange nice record that were created before the target record (refs #4011)

--- data/migrations/003_delete_orphan_records.php
+++ data/migrations/003_delete_orphan_records.php
@@ -9,7 +9,11 @@ class opLikePluginMigrationVersion3 extends opMigration
 {
   public function postUp()
   {
-    // nice.foreign_id に対応するレコードが存在しないものを一括で削除
+    /*
+     * 下記の条件に一致するレコードを一括で削除:
+     *   * nice.foreign_id に対応するレコードが存在しないもの
+     *   * nice.created_at が、nice.foreign_id に対応するレコードの created_at より過去の日時のもの
+     */
     $query = <<<'SQL'
 DELETE n FROM nice n
  LEFT JOIN activity_data ad ON n.foreign_table = "A" AND n.foreign_id = ad.id
@@ -18,6 +22,7 @@ DELETE n FROM nice n
  LEFT JOIN community_topic_comment tc ON n.foreign_table = "t" AND n.foreign_id = tc.id
  LEFT JOIN community_event_comment ec ON n.foreign_table = "e" AND n.foreign_id = ec.id
  WHERE COALESCE(ad.id, d.id, dc.id, tc.id, ec.id) IS NULL
+    OR n.created_at < COALESCE(ad.created_at, d.created_at, dc.created_at, tc.created_at, ec.created_at)
 SQL;
 
     $this->getConnection()->execute($query);
